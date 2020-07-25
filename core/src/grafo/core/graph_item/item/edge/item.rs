1ff23180e20624cd5b99d9510548a0fbbcae6ac9
ogata-k <ogtkzk712@gmail.com>
2020-07-25 10:45:30 +0900

    add: Edgeの基礎を記入

--- core/src/grafo/core/graph_item/item/edge/item.rs
+++ core/src/grafo/core/graph_item/item/edge/item.rs
@@ -1,14 +1,17 @@
 //! module for Edge item
 
 use crate::grafo::core::graph_item::GraphItemBase;
-use crate::util::alias::GroupId;
+use crate::util::alias::{GraphItemId, GroupId};
 use crate::util::item_base::ItemBase;
 use crate::util::kind::{GraphItemKind, HasGraphItemKind};
 
 /// Edge Item
 #[derive(Debug, Clone)]
 pub struct EdgeItem {
-    // TODO
+    // TODO Align can use RelativeAlign and AbsoluteAlign
+    belong_group_id: GroupId,
+    start: (GraphItemKind, GraphItemId),
+    end: (GraphItemKind, GraphItemId),
 }
 
 impl HasGraphItemKind for EdgeItem {
@@ -21,6 +24,16 @@ impl ItemBase for EdgeItem {}
 
 impl GraphItemBase for EdgeItem {
     fn get_belong_group_id(&self) -> GroupId {
-        unimplemented!()
+        self.belong_group_id
+    }
+}
+
+impl EdgeItem {
+    pub(crate) fn get_start_item_kind_id(&self) -> (GraphItemKind, GraphItemId) {
+        self.start
+    }
+
+    pub(crate) fn get_end_item_kind_id(&self) -> (GraphItemKind, GraphItemId) {
+        self.end
     }
 }
