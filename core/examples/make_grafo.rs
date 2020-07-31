6eabe35abc8b9426cc15e2e09b0237f88925d092
ogata-k <ogtkzk712@gmail.com>
2020-07-31 22:09:11 +0900

    mod: 回数と表示を修正

--- core/examples/make_grafo.rs
+++ core/examples/make_grafo.rs
@@ -1,10 +1,15 @@
+use std::time::SystemTime;
+
 use regrafilo_core::grafo::graph_item::node::NodeItemBuilder;
 use regrafilo_core::grafo::graph_item::GraphItemBuilderBase;
 use regrafilo_core::grafo::{GrafoBuilder, GrafoError};
 
-const ITERATE_COUNT: u32 = 5;
+const ITERATE_COUNT: u32 = 100;
+const SHOW: bool = false;
 
 fn main() {
+    println!("iter count: {}", ITERATE_COUNT);
+    let start = SystemTime::now();
     let mut graph = GrafoBuilder::new().build_with_default();
     let mut result = true;
     let mut errors: Vec<GrafoError> = Vec::new();
@@ -17,12 +22,16 @@ fn main() {
         result &= _result;
         errors.extend(_errors);
     }
+    let end = SystemTime::now();
+    println!("diff: {}ms", end.duration_since(start).unwrap().as_millis());
 
-    if result {
-        println!("{:?}", graph);
-    } else {
-        for error in errors {
-            println!("{}", error);
+    if SHOW {
+        if result {
+            println!("{:?}", graph);
+        } else {
+            for error in errors {
+                println!("{}", error);
+            }
         }
     }
 }
