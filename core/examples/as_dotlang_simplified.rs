54578e4e2329d4ca12d5a25a42e5530e6d9c36cf
ogata-k <ogtkzk712@gmail.com>
2020-09-12 13:33:10 +0900

    mod: グループを用いたグラフに変更し、target下の実行ファイルと同じ位置に結果を吐くように変更

--- core/examples/as_dotlang_simplified.rs
+++ core/examples/as_dotlang_simplified.rs
@@ -1,8 +1,14 @@
+use std::env::current_exe;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::process::Command;
+
 use regrafilo_core::grafo::graph_item::edge::EdgeItemBuilder;
+use regrafilo_core::grafo::graph_item::group::GroupItemBuilder;
 use regrafilo_core::grafo::graph_item::node::NodeItemBuilder;
 use regrafilo_core::grafo::graph_item::GraphItemBuilderBase;
 use regrafilo_core::grafo::{Grafo, NameStrGrafo, NameStrGrafoBuilder, NameStrGrafoError};
-use regrafilo_core::util::alias::GroupId;
+use regrafilo_core::util::alias::{GroupId, ItemId};
 use regrafilo_core::util::item_base::ItemBase;
 use regrafilo_core::util::kind::{GraphItemKind, HasGraphItemKind};
 use regrafilo_core::util::name_type::NameType;
@@ -12,41 +18,189 @@ const INDENT_WIDTH: usize = 4;
 fn main() {
     let graph = make_graph();
     let dotlang_graph = DotlangGraph::from(&graph);
-    println!("{}", dotlang_graph);
+
+    if cfg!(debug_assertions) {
+        println!("{}", dotlang_graph);
+    } else {
+        //
+        // assume that you install dot command(graphviz).
+        // if you not installed dot command, set false to use_convert for no convert action.
+        //
+        let use_convert = true;
+
+        // ../target/release/examples/example.dot
+        let mut path = current_exe().unwrap();
+        path.pop();
+        path.push("example.dot");
+        let mut file = OpenOptions::new()
+            .read(false)
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(path.clone())
+            .expect("open file fail");
+        file.write_all(format!("{}", dotlang_graph).as_bytes())
+            .expect("write all fail");
+        file.flush().expect("flush fail");
+        println!("created dotLang file");
+        if use_convert {
+            println!("converting to png...");
+            let mut converted_path = current_exe().unwrap();
+            converted_path.pop();
+            converted_path.push("example.png");
+
+            Command::new("dot")
+                .args(&[
+                    "-Tpng",
+                    "-o",
+                    converted_path.to_str().unwrap(),
+                    path.to_str().unwrap(),
+                ])
+                .status()
+                .unwrap_or_else(|e| panic!(e));
+            println!("finish convert");
+        }
+    }
 }
 
 type Graph = NameStrGrafo;
 type GraphBuilder = NameStrGrafoBuilder;
 type GraphError = NameStrGrafoError;
 
+fn print_error(result: bool, errors: Vec<GraphError>) {
+    if result {
+        for e in errors {
+            println!("warning:\t{}", e);
+        }
+    } else {
+        for e in errors {
+            eprintln!("error:\t{}", e);
+        }
+    }
+}
+
+fn to_item_name(kind: GraphItemKind, id: ItemId) -> impl NameType {
+    if kind == GraphItemKind::Group {
+        format!("cluster_{}_{}", kind, id)
+    } else {
+        format!("{}_{}", kind, id)
+    }
+}
+
 fn make_graph() -> Graph {
-    // TODO 今は仮置きなのでグループも使用したグラフに置き換える
-
     let mut graph: Graph = GraphBuilder::new().build_with_name_default_group("root group");
-
     let mut result = true;
-    let mut errors: Vec<GraphError> = Vec::new();
 
-    for i in 0..10 {
-        let mut node_builder = NodeItemBuilder::new();
+    for i in 1..=3 {
+        let mut group_builder = GroupItemBuilder::new();
         // when not use following method, set root group automatically
-        node_builder.set_belong_group("root group");
-        node_builder.set_name(format!("node {}", i));
-        let (_result, _errors) = graph.push_node(node_builder);
+        group_builder.set_belong_group("root group");
+        group_builder.set_name(format!("V_{}", i));
+        let (_result, _errors) = graph.push_group(group_builder);
         result &= _result;
-        errors.extend(_errors);
+        print_error(_result, _errors);
     }
 
-    for i in 0..10 {
+    for i in 1..=2 {
+        let mut node_builder = NodeItemBuilder::new();
+        // when not use following method, set root group automatically
+        node_builder.set_belong_group("V_1");
+        node_builder.set_name(format!("a_{}", i));
+        let (_result, _errors) = graph.push_node(node_builder);
+        result &= _result;
+        print_error(_result, _errors);
+    }
+
+    for i in 1..=2 {
+        let mut node_builder = NodeItemBuilder::new();
+        node_builder.set_belong_group("V_2");
+        node_builder.set_name(format!("b_{}", i));
+        let (_result, _errors) = graph.push_node(node_builder);
+        result &= _result;
+        print_error(_result, _errors);
+    }
+
+    for i in 1..=3 {
+        let mut node_builder = NodeItemBuilder::new();
+        node_builder.set_belong_group("V_3");
+        node_builder.set_name(format!("c_{}", i));
+        let (_result, _errors) = graph.push_node(node_builder);
+        result &= _result;
+        print_error(_result, _errors);
+    }
+
+    for i in 1..=2 {
+        let mut edge_builder = EdgeItemBuilder::new();
+        // when not use following method, set root group automatically
+        edge_builder.set_start_endpoint(GraphItemKind::Group, format!("V_{}", i));
+        edge_builder.set_end_endpoint(GraphItemKind::Group, format!("V_{}", i + 1));
+        let (_result, _errors) = graph.push_edge(edge_builder);
+        result &= _result;
+        print_error(_result, _errors);
+    }
+
+    let mut edge_builder = EdgeItemBuilder::new();
+    edge_builder.set_belong_group("V_2");
+    edge_builder.set_start_endpoint(GraphItemKind::Node, "b_1");
+    edge_builder.set_end_endpoint(GraphItemKind::Node, "b_2");
+    let (_result, _errors) = graph.push_edge(edge_builder);
+    result &= _result;
+    print_error(_result, _errors);
+
+    let mut edge_builder = EdgeItemBuilder::new();
+    edge_builder.set_belong_group("V_3");
+    edge_builder.set_start_endpoint(GraphItemKind::Node, "c_2");
+    edge_builder.set_end_endpoint(GraphItemKind::Node, "c_2");
+    let (_result, _errors) = graph.push_edge(edge_builder);
+    result &= _result;
+    print_error(_result, _errors);
+
+    for (is_reverse, i, j) in [(true, 1, 3), (false, 1, 2), (true, 2, 2), (false, 2, 3)].iter() {
         let mut edge_builder = EdgeItemBuilder::new();
         // when not use following method, set root group automatically
         edge_builder.set_belong_group("root group");
-        edge_builder.set_name(format!("edge {}", i));
-        edge_builder.set_start_endpoint(GraphItemKind::Node, format!("node {}", (2 * i) % 10));
-        edge_builder.set_end_endpoint(GraphItemKind::Node, format!("node {}", (7 * (i + 1)) % 10));
+        if !*is_reverse {
+            edge_builder.set_start_endpoint(GraphItemKind::Node, format!("a_{}", i));
+            edge_builder.set_end_endpoint(GraphItemKind::Group, format!("V_{}", j));
+        } else {
+            edge_builder.set_start_endpoint(GraphItemKind::Group, format!("V_{}", j));
+            edge_builder.set_end_endpoint(GraphItemKind::Node, format!("a_{}", i));
+        }
+
         let (_result, _errors) = graph.push_edge(edge_builder);
         result &= _result;
-        errors.extend(_errors);
+        print_error(_result, _errors);
+    }
+
+    for (is_reverse, i, j) in [
+        (false, 1, 1),
+        (false, 1, 2),
+        (true, 1, 3),
+        (true, 2, 1),
+        (true, 2, 2),
+        (false, 2, 3),
+    ]
+    .iter()
+    {
+        let mut edge_builder = EdgeItemBuilder::new();
+        edge_builder.set_belong_group("root group");
+        if !*is_reverse {
+            edge_builder.set_start_endpoint(GraphItemKind::Node, format!("b_{}", i));
+            edge_builder.set_end_endpoint(GraphItemKind::Node, format!("c_{}", j));
+        } else {
+            edge_builder.set_start_endpoint(GraphItemKind::Node, format!("c_{}", j));
+            edge_builder.set_end_endpoint(GraphItemKind::Node, format!("b_{}", i));
+        }
+
+        let (_result, _errors) = graph.push_edge(edge_builder);
+        result &= _result;
+        print_error(_result, _errors);
+    }
+
+    if result {
+        println!("all item build success");
+    } else {
+        eprintln!("build fail item exist");
     }
 
     graph
@@ -79,30 +233,13 @@ impl<'a, Name: NameType + Default> DotlangGraph<'a, Name> {
             .get_root_group_item()
             .expect("root group is not initialized yet.")
             .get_item_id();
+
         // In future, be able to be label instead of item_name for label argument.
         writeln!(
             f,
-            "digraph Grafo {{\n{}label = \"{}\";",
-            " ".repeat(indent_count * INDENT_WIDTH),
-            resolver
-                .get_graph_item_name_by(GraphItemKind::Group, root_id, root_id)
-                .expect("not found root group")
+            "digraph Grafo{{\n{}graph[layout=fdp, compound=true, label=\"example graph\", sep=0.3];",
+            " ".repeat(indent_count * INDENT_WIDTH)
         )?;
-
-        writeln!(f, "\n{}// Nodes", " ".repeat(indent_count * INDENT_WIDTH))?;
-        for (item_id, item) in self.grafo.get_node_item_iter_limit_by_group_id(root_id) {
-            // In future, be able to be label instead of item_name for label argument.
-            writeln!(
-                f,
-                "{}{}_{}[label=\"{}\"];",
-                " ".repeat(indent_count * INDENT_WIDTH),
-                item.get_kind(),
-                item_id,
-                resolver
-                    .get_graph_item_name_by_item(item)
-                    .unwrap_or_else(|| &name_default)
-            )?;
-        }
 
         let child_ids = resolver.get_child_ids(root_id);
         if !child_ids.is_empty() {
@@ -117,6 +254,20 @@ impl<'a, Name: NameType + Default> DotlangGraph<'a, Name> {
             }
         }
 
+        writeln!(f, "\n{}// Nodes", " ".repeat(indent_count * INDENT_WIDTH))?;
+        for (item_id, item) in self.grafo.get_node_item_iter_limit_by_group_id(root_id) {
+            // In future, be able to be label instead of item_name for label argument.
+            writeln!(
+                f,
+                "{}{}[label=\"{}\"];",
+                " ".repeat(indent_count * INDENT_WIDTH),
+                to_item_name(item.get_kind(), *item_id),
+                resolver
+                    .get_graph_item_name_by_item(item)
+                    .unwrap_or_else(|| &name_default)
+            )?;
+        }
+
         writeln!(f, "\n{}// Edges", " ".repeat(indent_count * INDENT_WIDTH))?;
         for (_, item) in self.grafo.get_edge_item_iter_limit_by_group_id(root_id) {
             let start = item.get_start_endpoint();
@@ -124,12 +275,10 @@ impl<'a, Name: NameType + Default> DotlangGraph<'a, Name> {
             // In future, be able to be label instead of item_name for label argument.
             writeln!(
                 f,
-                "{}{}_{} -> {}_{}[label=\"{}\"];",
+                "{}{} -> {}[label=\"{}\"];",
                 " ".repeat(indent_count * INDENT_WIDTH),
-                start.get_kind(),
-                start.get_item_id(),
-                end.get_kind(),
-                end.get_item_id(),
+                to_item_name(start.get_kind(), start.get_item_id()),
+                to_item_name(end.get_kind(), end.get_item_id()),
                 resolver
                     .get_graph_item_name_by_item(item)
                     .unwrap_or_else(|| &name_default)
@@ -147,16 +296,19 @@ impl<'a, Name: NameType + Default> DotlangGraph<'a, Name> {
     ) -> std::fmt::Result {
         let name_default = Name::default();
         let resolver = self.grafo.resolver();
+
         // In future, be able to be label instead of item_name for label argument.
         writeln!(
             f,
-            "{}subgraph subgraph_{} {{\n{}label=\"{}\";\n",
+            "{}subgraph {} {{\n{}graph[label=\"{}\"];\n{}node [style=filled];\n{}color=black;",
             " ".repeat(indent_count * INDENT_WIDTH),
-            child_id,
+            to_item_name(GraphItemKind::Group, child_id),
             " ".repeat((indent_count + 1) * INDENT_WIDTH),
             resolver
                 .get_graph_item_name_by(GraphItemKind::Group, parent_id, child_id)
-                .unwrap_or_else(|| &name_default)
+                .unwrap_or_else(|| &name_default),
+            " ".repeat((indent_count + 1) * INDENT_WIDTH),
+            " ".repeat((indent_count + 1) * INDENT_WIDTH)
         )?;
 
         writeln!(
@@ -168,16 +320,14 @@ impl<'a, Name: NameType + Default> DotlangGraph<'a, Name> {
             // In future, be able to be label instead of item_name for label argument.
             writeln!(
                 f,
-                "{}{}_{}[label=\"{}\"];",
+                "{}{}[label=\"{}\"];",
                 " ".repeat((indent_count + 1) * INDENT_WIDTH),
-                item.get_kind(),
-                item_id,
+                to_item_name(item.get_kind(), *item_id),
                 resolver
                     .get_graph_item_name_by_item(item)
                     .unwrap_or_else(|| &name_default)
             )?;
         }
-        writeln!(f)?;
 
         let child_ids = resolver.get_child_ids(child_id);
         if !child_ids.is_empty() {
@@ -203,12 +353,10 @@ impl<'a, Name: NameType + Default> DotlangGraph<'a, Name> {
             // In future, be able to be label instead of item_name for label argument.
             writeln!(
                 f,
-                "{}{}_{} -> {}_{}[label=\"{}\"];",
+                "{}{} -> {}[label=\"{}\"];",
                 " ".repeat((indent_count + 1) * INDENT_WIDTH),
-                start.get_kind(),
-                start.get_item_id(),
-                end.get_kind(),
-                end.get_item_id(),
+                to_item_name(start.get_kind(), start.get_item_id()),
+                to_item_name(end.get_kind(), end.get_item_id()),
                 resolver
                     .get_graph_item_name_by_item(item)
                     .unwrap_or_else(|| &name_default)
