1b1565aca8ea67292ebc82d88d29c49370fa24d6
ogata-k <ogtkzk712@gmail.com>
2020-09-26 10:50:15 +0900

    clean: graph_item_baseと順番を合わせた

--- core/src/util/item_base.rs
+++ core/src/util/item_base.rs
@@ -7,17 +7,11 @@ use crate::grafo::Resolver;
 use crate::util::alias::ItemId;
 use crate::util::name_type::NameType;
 
-/// trait for base of builder for item
-pub trait ItemBuilderBase<Name: NameType> {
-    /// result when build success
-    type Item: ItemBase;
-    /// result have this error when build success and fail with error
-    type ItemError: ItemErrorBase<Name>;
+/// base trait for item
+pub trait ItemBase {
+    /// getter for item id
+    fn get_item_id(&self) -> ItemId;
 }
-
-/// alias for result of builder's build method
-pub(crate) type ItemBuilderResult<Name, Item, ItemOption> =
-    (Option<(Item, ItemOption)>, Vec<GrafoError<Name>>);
 
 /// add build method to builder
 pub(crate) trait HasItemBuilderMethod<Name: NameType>: ItemBuilderBase<Name> {
@@ -31,11 +25,17 @@ pub(crate) trait HasItemBuilderMethod<Name: NameType>: ItemBuilderBase<Name> {
     ) -> ItemBuilderResult<Name, Self::Item, Self::ItemOption>;
 }
 
-/// base trait for item
-pub trait ItemBase {
-    /// getter for item id
-    fn get_item_id(&self) -> ItemId;
+/// trait for base of builder for item
+pub trait ItemBuilderBase<Name: NameType> {
+    /// result when build success
+    type Item: ItemBase;
+    /// result have this error when build success and fail with error
+    type ItemError: ItemErrorBase<Name>;
 }
+
+/// alias for result of builder's build method
+pub(crate) type ItemBuilderResult<Name, Item, ItemOption> =
+    (Option<(Item, ItemOption)>, Vec<GrafoError<Name>>);
 
 /// base trait for item builder's error
 pub trait ItemErrorBase<Name: NameType>: Eq + Error + Into<GrafoError<Name>> {}
